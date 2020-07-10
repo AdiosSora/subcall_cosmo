8d8c7c782067e6fc6ff6dc2c4132ffe84a4306c6
ikazemu <s3a2@KCSF0418>
2020-07-10 12:27:12 +0900

    更新

--- index.php
+++ index.php
@@ -9,7 +9,19 @@
 <body>
     <div class="container">
         <h1 class="heading">Welcome to Subcall</h1>
-        <a href="join.php" class="btn-square-shadow">会議へ参加</a>
+        <script>
+          function golink(){
+             location.href="login.php";
+          }
+        </script>
+        <button type="button" onclick="golink()">ログイン</button></br>
+        <form action="login.php" method="post">
+          部屋番号を入力してください
+          <input type="text" name="roomId" size="30" maxlength="20"></br>
+          言語を選択してください(Select a language)
+          <select name="language">
+            <option value="ja">日本語</option>
+            <option value="english">英語</option>
         </p>
 </body>
 </html>
