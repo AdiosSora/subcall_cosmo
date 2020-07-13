7c35033fd8011acea0d18309deffed18c1d44dc7
adiossora <adiosssora1202@gmail.com>
2020-07-13 14:19:34 +0900

    調整

--- index.php
+++ index.php
@@ -19,13 +19,21 @@
 
             いいいいいいいいいいいいいいいいいいいいいいいいいい
           </div>
-          <div class="col s12 m6 center"><form>
-            <button type="button" onclick="golink()">ログイン</button></br>
+          <div class="col s12 m6 center">
+          <div class="lighten-5">
+            <form>
+              <button type="button" onclick="golink()">ログイン</button></br>
 
-            <button type="button" onclick="room_make()">部屋を作る</button></br>
+              <button type="button" onclick="room_make()">部屋を作る</button></br>
 
-            <button type="button" onclick="room_in()">部屋に入る</button></br>
-          </form>
+              <button type="button" onclick="room_in()">部屋に入る</button></br>
+            </form>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">textsms</i>
+              <input type="text" id="autocomplete-input" class="autocomplete">
+              <label for="autocomplete-input">Autocomplete</label>
+          </div>
+        </div>
             <script>
               function golink(){
                  location.href="login.php";
--- js/script.js
+++ js/script.js
@@ -118,3 +118,21 @@ const Peer = window.Peer;
 
   peer.on('error', console.error);
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.autocomplete');
+   var instances = M.Autocomplete.init(elems, options);
+ });
+
+
+ // Or with jQuery
+
+ $(document).ready(function(){
+   $('input.autocomplete').autocomplete({
+     data: {
+       "Apple": null,
+       "Microsoft": null,
+       "Google": 'https://placehold.it/250x250'
+     },
+   });
+ });
