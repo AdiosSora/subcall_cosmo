8ff15ec6a962c5cf43ccf2fc269fe07d8c7174b2
adiossora <adiosssora1202@gmail.com>
2020-07-14 13:46:43 +0900

    調整

--- register.php
+++ register.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 
 <html lang="ja">
-<<<<<<< HEAD
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -26,10 +25,10 @@
               <div class="card-content">
                 <form method="post" action="register_check.php" id="check" class="pw-form-container">
                     <h1>会員登録画面</h1><br/>
+                    <label for="name"><span class="required_mark">必須</span> お名前</label>
+                    <input type="text" name="name" id="name" size="30" maxlength="20" placeholder="UserName" autocomplete="off"></p>
 
-                    <p><input type="text" name="name" id="name" size="30" maxlength="20" placeholder="UserName" autocomplete="off"></p>
-
-                    <p><input type="password" name="pass" id="pass" size="30" maxlength="20" placeholder="Password" autocomplete="off">
+                    <input type="password" name="pass" id="pass" size="30" maxlength="20" placeholder="Password" autocomplete="off">
                     <span class="helper-text" data-error="wrong" data-success="right"><i toggle="#password-field" class="mdi mdi-eye toggle-password"></i></span></p>
 
                     <p><input type="password" name="pass2" id="pass2" size="30" maxlength="20" placeholder="Password  (check)" autocomplete="off">
@@ -62,55 +61,6 @@
         </div>
         <br><br>
 
-=======
-  <head>
-      <meta charset="utf-8"/>
-      <link rel="stylesheet" href="css/register.css">
-      <title>Acount regist</title>
-  </head>
-
-  <body>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      <script src="js/jquery.validate.js"></script>
-      <script src="js/jquery.validate.min.js"></script>
-      <script src="js/register.js"></script>
-
-
-      <div class="pw-form">
-          <form method="post" action="register_check.php" id="check" class="pw-form-container">
-              <h1>会員登録画面</h1><br/>
-
-              <p><input type="text" name="name" id="name" size="30" maxlength="30" placeholder="UserName" autocomplete="off"></p>
-
-              <p><input type="password" name="pass" id="pass" size="30" maxlength="30" placeholder="Password" autocomplete="off"></p>
-                 <span class="field-icon">
-                   <i toggle="#password-field" class="mdi mdi-eye toggle-password"></i>
-                 </span>
-
-              <p><input type="password" name="pass2" id="pass2" size="30" maxlength="30" placeholder="Password  (check)" autocomplete="off"></p>
-                 <span class="field-icon">
-                   <i toggle="#password-field" class="mdi mdi-eye toggle-password"></i>
-                 </span>
-
-              <script>
-                $(".toggle-password").click(function() {
-                  $(this).toggleClass("mdi-eye mdi-eye-off");
-
-                  var input = $(this).parent().prev("input");
-                  if (input.attr("type") == "password") {
-                    input.attr("type", "text");
-                  } else {
-                    input.attr("type", "password");
-                  }
-                });
-              </script>
-
-              <p><input type="email" name="address" id="address" size="30" maxlength="100" placeholder="E-mail" autocomplete="off"></p>
-
-              <p><button type="button" onclick="history.back()">戻る</button></p>
-              <p><button type="submit" name="regist" value="登録">確認</button></p>
-          </form>
->>>>>>> 759231a45b1418f683d10c1381cced48eddcd61f
       </div>
     </div>
     <div class="parallax" style="background:#999999;"></div>
