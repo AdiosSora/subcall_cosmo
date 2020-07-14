cbe0302099b619e42f682e62e86edfb70ed6f7f9
GABA <gabagaba@mail.com>
2020-07-14 11:05:40 +0900

    aiueo

--- register.php
+++ register.php
@@ -2,46 +2,62 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8"/>
+
     <title>Acount regist</title>
+    <link href="validationEngine.jquery.css" rel="stylesheet" type="text/css" /> <script src="jquery.validationEngine.js"></script> <script src="jquery.validationEngine-ja.js"></script>
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+      <script>
+          jQuery(document).ready(function(){
+          jQuery("#register").validationEngine();
+          });
+      </script>
+
   </head>
+
   <body>
     <h1>アカウント登録画面</h1>
+    <form method="post" action="register_check.php" id="register">
 
-    <form method="post" action="register_check.php">
+      アカウント名<br>
+     <input type="text" id="text3_1" class="validate[required] text-input" /><br><br>
 
-    アカウント名
-    <input type="text" name="name" size="30" maxlength="20" required> <br><br>
+      メールアドレス<br>
+       <input type="text" id="text3_3_1" class="validate[required,custom[email]] text-input" /><br><br>
 
-    メールアドレス
-    <input type="text" name="address" size="30" maxlength="100" required> <br><br>
+      パスワード<br>
+      <input class="validate[required] text-input" type="password" id="passwd" /><br><br>
 
-    パスワード
-    <input type="password" name="pass" id="word" size="30" maxlength="30"><br><br>
+      パスワードをもう一度入力してください<br>
+      <input class="validate[required,equals[passwd]] text-input" type="password" id="re_passwd" /><br><br>
+      <!--<input type="password" name="pass" size="30" id="word" maxlength="30" class="validate[password] text-input" data-errormessage-value-missing="半角英数大文字１字以上で入力してください。" data-prompt-position="topLeft"><br><br>
+      -->
 
-    パスワードをもう一度入力してください
-    <input type="password" name="pass2" id="word2" size="30" maxlength="30"><br><br>
+    <!--input type="password" name="pass2" size="30" id="word2" maxlength="30" class="validate[password] text-input" data-errormessage-value-missing="半角英数大文字１字以上で入力してください。" data-prompt-position="topLeft"><br><br>
+    -->
 
     <button type="button" onclick="history.back()">戻る</button>
     <button type="submit" onclick="return_test" id="b1" name="register" value="登録">確認</button>
+  </form>
+
 
     <script>
 
-          function return_test()
-          {
-            if(document.getElementById("word").value != document.getElementById("word2").value)
-            {
-              alert("パスワードが一致していません");
-              return false;
-            }
-            else
-            {
-              return true;
-            }
-          }
+          //function return_test()
+          //{
+            //if(document.getElementById("word").value != document.getElementById("word2").value)
+            //{
+              //alert("パスワードが一致していません");
+              //return false;
+            //}
+            //else
+            //{
+              //return true;
+            //}
+          //}
 
     </script>
 
-    </form>
+
 
   </body>
 </html>
