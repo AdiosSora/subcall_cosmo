182651e2a1f8afbd8a0af6ce834d83f6413a684f
AdiosSora <adiossora1202@gmail.com>
2020-07-16 00:17:28 +0900

    リンク修正

--- nav.php
+++ nav.php
@@ -1,7 +1,10 @@
 <!--ようこそゲスト様or○○（ログインユーザー）様-->
 <?php
+try{
 session_start();
 session_regenerate_id(true);
+} catch (Exception $e) {
+}
 // test用データ、削除予定
 //$_SESSION['bool']= null; // 「null」,「1」いずれか代入で分岐可能
 //$_SESSION['regist_name'] = '太郎';
@@ -9,7 +12,7 @@ session_regenerate_id(true);
 ?>
 <nav class="white" role="navigation">
   <div class="nav-wrapper container">
-    <a id="logo-container" href="./index.php" class="brand-logo">
+    <a id="logo-container" href="/index.php" class="brand-logo">
       <div>
         <object id="front-page-logo" class="responsive-img" width="170px" type="image/svg+xml" data="STABLE_logo.svg">
           Your browser does not support SVG.
@@ -23,8 +26,8 @@ session_regenerate_id(true);
         // セッション名は統一
         if(isset($_SESSION['bool']) == false)
         {
-          print '<li><a href="login.php">Log in</a></li>';
-          print '<li><a href="register.php"Sign up</a></li>';
+          print '<li><a href="/account/login.php">Log in</a></li>';
+          print '<li><a href="/account/register.php"Sign up</a></li>';
           print '<br />';
         }
         else
