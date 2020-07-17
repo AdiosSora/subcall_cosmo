f9b1b1dbd77a5d0965b19333c67b1f4ac9091ee4
GABA-alt <noraneko1780@outlook.jp>
2020-07-18 00:37:49 +0900

    profile

--- account/profile.php
+++ account/profile.php
@@ -2,7 +2,11 @@
 <?php
 try{
 session_start();
-session_regenerate_id(true);
+if(isset($_SESSION['bool'])==false){
+  print'ログインされていません。<br/>';
+  print'<a href="login.php">ログイン画面へ</a>';
+  exit();
+}
 } catch (Exception $e) {
 }
 
@@ -13,32 +17,42 @@ session_regenerate_id(true);
             <meta charset="utf-8"/>
         </head>
         <body>
-          <form method="post" action profile_done.php enctype="multipart/form-data" >
-            画像ファイル<br/>
-            <input type="file"  name="pic"><br/>
-
+          <h1>Profile</h1>
 
             名前：<?php print $_SESSION['regist_name'];?> <br/><br/>
-            メールアドレス： <?php print $_SESSION['regist_address'];?> <br/><br/>
+            E-mail： <?php print $_SESSION['regist_address'];?> <br/><br/>
 
-            <!-- <?php if($flag){
-                    print'生年月日： 未設定<br/>';
-                    print'国： 未設定<br/>';
-                    print'性別: 未設定<br/>';
-                  }else {
+              <?php
+               print'生年月日 : ';
+                 if(isset($_SESSION['borne'])==false){
+                     print '未設定<br><br>';
+                 }else {
+                     print $_SESSION['borne'];
+                 }
 
-                  }
+               print'国 : ';
+                 if(isset($_SESSION['country'])==false){
+                   print '未設定<br><br>';
+                 }else {
+                   print $_SESSION['country'];
+                 }
 
-                ?> -->
+               print'性別 : ';
+                 if(isset($_SESSION['gender'])==false){
+                   print '未設定<br><br>';
+                 }else {
+                   print $_SESSION['gender'];
+                 }
 
+              ?>
 
-
-
-
-
-              <input type="button" a href ="register_update" name="update" value="編集"><br/>
-                <a href="delete.php">退会</a>
-                <a class= href="...php"></a>
+              <a href="register_update.php">
+                <button type="button">編集</button>
+              </a>
+              <br>
+              <br>
+              <br>
+              <a href="delete.php">退会</a>
           </form>
 
         </body>
