ad81511be02b422aab50a5a3b1d72e5580cdb33f
adiossora <adiosssora1202@gmail.com>
2020-07-27 15:56:26 +0900

    更新

--- join.php
+++ join.php
@@ -42,74 +42,36 @@
       </div>
 
       <p class="meta" id="js-meta"></p>
-<<<<<<< HEAD
-      </div>
-    音声認識ログ<input type="text" name="speechText">
-    </div>
-=======
-<<<<<<< HEAD
       <div id="test"></div>
     </div>
-    音声認識ログ<input type="text" name="speechText">
-    <script src="//cdn.webrtc.ecl.ntt.com/skyway-latest.js"></script>
-    <script src="/js/script.js"></script>
-    </div>
-=======
-      <p><iframe src="/test/speech.html"></iframe></p>
-      </div>
->>>>>>> 4e6726ee9f58b565e8dfb528e757fa6018dce763
->>>>>>> 05f5a88d047b13a0cd0fea2d28b19e8c99aca08f
+    <script>
+    window.setTimeout(() => {
+        const speech = new webkitSpeechRecognition();
+        speech.lang = 'ja-JP';
+
+        const content = document.getElementById('videoSub');
+
+        // 音声認識をスタート
+        speech.start();
+
+        //音声自動文字起こし機能
+        speech.onresult = function (e) {
+            speech.stop();
+            if (e.results[0].isFinal) {
+                var autotext = e.results[0][0].transcript
+                content.innerHTML += '<div>' + autotext + '</div>';
+            }
+        }
+
+        speech.onend = () =>
+        {
+            speech.start()
+        };
+      },1000);
+
+    </script>
   </body>
   <script src="//cdn.webrtc.ecl.ntt.com/skyway-latest.js"></script>
   <script src="/js/script.js"></script>
-  <script>
-      const speech = new webkitSpeechRecognition();
-      speech.lang = 'ja-JP';
 
-      const content = document.getElementById('test');
-
-      start_btn.addEventListener('click', function () {
-          // 音声認識をスタート
-          speech.start();
-      });
-
-      //音声自動文字起こし機能
-      speech.onresult = function (e) {
-          speech.stop();
-          if (e.results[0].isFinal) {
-              var autotext = e.results[0][0].transcript
-              content.innerHTML += '<div>' + autotext + '</div>';
-          }
-      }
-
-      speech.onend = () =>
-      {
-          speech.start()
-      };
-
-  </script>
-</html>
-<script>
-window.setTimeout(() => {
-    const speech = new webkitSpeechRecognition();
-    speech.lang = 'ja-JP';
-
-    speech.start();
-    const content = document.getElementById('videoSub');
-
-    //音声自動文字起こし機能
-    speech.onresult = function (e) {
-        speech.stop();
-        if (e.results[0].isFinal) {
-            var autotext = e.results[0][0].transcript
-            content.innerHTML += autotext;
-        }
-    }
-
-    speech.onend = () =>
-    {
-        speech.start();
-    };
-  },3000);
-
-</script>
+  </html>
