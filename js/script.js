74623e1f97ef0af08f426a8429f455460afe4360
adiossora <adiosssora1202@gmail.com>
2020-08-28 16:40:19 +0900

    Commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* eslint-disable require-jsdoc */
 $(function() {
-  // Peer object
+  // Peerオブジェクトを確立
   const peer = new Peer({
     key:   '766085bc-041a-4889-ba90-b8fda1a4615f',
     debug: 3,
@@ -9,22 +9,25 @@ $(function() {
   const localText = document.getElementById('chat-textarea');
   const messages = document.getElementById('chat-text');
   const sub_messages = document.getElementById('sub-text');
+  var bg_chat_color = 0;
 
   let localStream;
   let room;
+
+  //peerを確立
   peer.on('open', () => {
     $('#my-id').text(peer.id);
-    // Get things started
     step1();
   });
 
+  //接続に問題があった場合
   peer.on('error', err => {
     alert(err.message);
     // Return to step 2 if error occurs
     step2();
   });
 
-
+  //ルーム入室ボタンが押された場合
   $('#make-call').on('submit', e => {
     e.preventDefault();
     // Initiate a call!
@@ -42,34 +45,42 @@ $(function() {
     step4();
   });
 
+  //ルーム退出ボタンが押された場合
   $('#end-call').on('click', () => {
     room.close();
     step2();
   });
 
+  //再接続ボタンが押された場合
   $('#step1-retry').on('click', () => {
     $('#step1-error').hide();
     step1();
   });
 
+  //チャット送信ボタンが押された場合
   $('#btn-send').on('click', () => {
     onClickSend();
   });
 
+  //チャット送信関数
   function onClickSend() {
-    // Send message to all of the peers in the room via websocket
-    console.log('チャット送信');
-    room.send('1'+localText.value);
-    $("#chat-text").append($(
-      '<div class="msg_content">' +
-      '<div class="msg-icon"><img src="../images/icon2.png"></div>' +
-      '<div class="msg-text">' +
-      '<div class="msg-name"><strong>' + peer.id + '</strong></div>'+
-      '<div class="msg-content">' + localText.value + '</div>' +
-      '</div></div>'));
-    localText.value='';
-
+    if(localText.value!=''){
+      console.log('チャット送信');
+      room.send('1'+localText.value);
+      $("#chat-text").append($(
+        '<div class="msg_content bg-' + bg_chat_color + '">' +
+        '<div class="msg-icon"><img src="../images/icon2.png"></div>' +
+        '<div class="msg-text">' +
+        '<div class="msg-name"><strong>' + peer.id + '</strong></div>'+
+        '<div class="msg-content">' + localText.value + '</div>' +
+        '<div class="msg-date">' + getNow() + '</div>' +
+        '</div></div>'));
+      (bg_chat_color==0)?bg_chat_color=1:bg_chat_color=0;
+      localText.value='';
+    }
   }
+
+  //字幕送信関数
   function onSubSend(subtext) {
     // Send message to all of the peers in the room via websocket
     console.log('字幕送信');
@@ -77,11 +88,12 @@ $(function() {
     localText.value = '';
   }
 
-  // set up audio and video input selectors
+  //使用するビデオとオーディオを選択するための定数
   const audioSelect = $('#audioSource');
   const videoSelect = $('#videoSource');
   const selectors = [audioSelect, videoSelect];
 
+  //デバイス選択処理
   navigator.mediaDevices.enumerateDevices()
     .then(deviceInfos => {
       const values = selectors.map(select => select.val() || '');
@@ -119,6 +131,7 @@ $(function() {
       audioSelect.on('change', step1);
     });
 
+  //デバイスの指定とローカルストリームの取得を行う関数
   function step1() {
     // Get audio/video stream
     const audioSource = $('#audioSource').val();
@@ -143,6 +156,7 @@ $(function() {
     });
   }
 
+  //ルーム入室時の初期化関数
   function step2() {
     $('#their-videos').empty();
     $('#step1, #step3').hide();
@@ -150,6 +164,7 @@ $(function() {
     $('#join-room').focus();
   }
 
+  //ルーム内のリスナーを設定している関数
   function step3() {
     // Wait for stream on the call, then set peer video display
     room.on('stream', stream => {
@@ -188,12 +203,14 @@ $(function() {
       if(result_num == '1'){
         console.log('データ受け取り1発火');
         $("#chat-text").append($(
-          '<div class="msg_content">' +
+          '<div class="msg_content bg-' + bg_chat_color + '"">' +
           '<div class="msg-icon"><img src="../images/icon1.png"></div>' +
           '<div class="msg-text">' +
           '<div class="msg-name"><strong>' + `${src}` + '</strong></div>'+
           '<div class="msg-content">' + `${result_message}\n` + '</div>' +
+          '<div class="msg-date">' + getNow() + '</div>' +
           '</div></div>'));
+          (bg_chat_color==0)?bg_chat_color=1:bg_chat_color=0;
       }else
       if(result_num == '2'){
         console.log('データ受け取り2発火');
@@ -202,11 +219,11 @@ $(function() {
         var subtext_area = document.getElementById(`${src}`);
         console.log(`${subtext_area}`);
         sub_messages.innerHTML += result_message.substring(peerID_length);
-
         console.log(result_message.substring(result_message));
         console.log(result_message.substring(peerID_length));
       }
     });
+
 
     room.on('peerLeave', peerId => {
       $('.video_' + peerId).remove();
@@ -215,8 +232,9 @@ $(function() {
     $('#step1, #step2').hide();
     $('#step3').show();
   }
-  function step4(){
 
+  //音声認識を自動で行う関数
+  function step4(){
     const speech = new webkitSpeechRecognition();
     speech.lang = 'ja-JP';
     //speech.start();
@@ -241,5 +259,13 @@ $(function() {
       console.log('認識再開');
         speech.start()
     };
+  }
+  //時分を出力する関数
+  function getNow() {
+  	var now = new Date();
+  	var hour = now.getHours();
+  	var min = now.getMinutes();
+  	var s =hour + ":" + min;
+  	return s;
   }
 });
