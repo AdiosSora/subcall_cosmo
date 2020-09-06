fa17e0e04f7aa3aa834bcda148216698358db118
AdiosSora <adiossora1202@gmail.com>
2020-09-06 13:22:01 +0900

    重複指定などの修正

--- header.php
+++ header.php
@@ -1,19 +1,13 @@
 <!-- CSS  -->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<link href="/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<!--Import Google Icon Font-->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<!--Let browser know website is optimized for mobile-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<!--  Scripts-->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="/js/materialize.js"></script>
+<script src="/js/materialize.min.js"></script>
 <script src="/js/init.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.0"></script>
-<!---<script src="https://cdn.jsdelivr.net/npm/vue@2.6.0"></script>--->
 
 <!--- favicon --->
 <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
