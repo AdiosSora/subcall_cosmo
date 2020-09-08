36e8614b77b46cab9f3f9162a2a7b686562eaea1
adiossora <adiosssora1202@gmail.com>
2020-09-08 14:05:31 +0900

    フレンド一覧を移行

--- account/friend/friend.php
+++ account/friend/friend.php
@@ -130,7 +130,61 @@
 							</form>
 
 							<a href="../../index.php">トップ画面へ</a>
-						<?php
+              <?php
+              // DB接続(mysql, xampp)
+              $dbh = get_DBobj();
+              $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+              // flag=true で自身以外の番号と名前を取得
+              $sql = 'SELECT number, name FROM account
+                      WHERE (number IN(
+                      SELECT user_number FROM friendlist
+                      WHERE flag=true AND (user_number=? OR friend_number=?)
+                    ) OR number IN(
+                      SELECT friend_number FROM friendlist
+                      WHERE flag=true AND (user_number=? OR friend_number=?)
+                    )) AND number NOT LIKE ?
+                      ORDER BY number ';
+
+              $stmt = $dbh->prepare($sql);
+              $data2[] = $user_num;
+              $data2[] = $user_num;
+              $data2[] = $user_num;
+              $data2[] = $user_num;
+              $data2[] = $user_num;
+              $stmt->execute($data2);
+
+              $dbh = null;
+
+              ?>
+              <table border="1">
+                <caption>フレンド一覧</caption>
+                <tr>
+                  <th>会員番号</th>
+                  <th>会員名</th>
+                  <th>削除の実行</th>
+                </tr>
+                <?php
+                while(true){
+
+                  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                  if($rec == false){
+                    break;
+                  }
+                  print '<tr>';
+                  print '<td>'.$rec['number'].'</td>';
+                  print '<td>'.$rec['name'].'</td>';
+                  print '<form method="post" action="friend_list_check.php">';
+                  print '<input type="hidden" name="list_num" value="'.$rec['number'].'">';
+                  print '<input type="hidden" name="list_name" value="'.$rec['name'].'">';
+                  print '<td align="center">';
+                  print '<input type="submit" name="list_check" value="フレンド削除" >';
+                  print '</form>';
+                  print '</td>';
+                  print '</tr>';
+                  }
+              print '</table>';
 						}
 						?>
 				</div>
